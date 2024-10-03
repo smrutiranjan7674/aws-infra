8d93d4e87d83c67b4dac6bf90a8282c2e55898ae
Smruti Ranjan Swain <smrutiranjanswain@Smrutis-MacBook-Pro.local>
2024-10-03 20:45:49 +0200

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "ebs" {
   source            = "git::https://github.com/smrutiranjan7674/aws-tfmodule-ebs.git?ref=main"
-  availability_zone = "eu-west-1"
+  availability_zone = "eu-west-1a"
   size              = 20
   volume_type       = "gp2"
   #device_name       = "/dev/sdh"
